3caed26b9f9fb18a1196a0b4d9f6b0b658ad126b
Oleksandr Zakotianskyi <zakotianskiy.external@taxy.io>
2022-11-27 19:52:59 +0100

    ref visitor

--- 23. Visitor/2. Reflective Visitor.py
+++ 23. Visitor/2. Reflective Visitor.py
@@ -1,3 +1,79 @@
 """
-
+Improving the Intrusive approach with
+Separate class for printing
 """
+from __future__ import annotations
+
+import inspect
+from abc import ABC
+from typing import List, Union, Any
+import logging
+
+logger = logging.Logger(__name__)
+
+
+class Expression(ABC):
+    """Class to what we will dynamically add functionality"""
+    pass
+
+
+class DoubleExpression(Expression):  # add inheritance
+    def __init__(self, value):
+        self.value = value
+
+
+class AdditionExpression(Expression):  # add inheritance
+    def __init__(self, left: Union[DoubleExpression, Any], right: Union[DoubleExpression, Any]):
+        self.left = left
+        self.right = right
+
+
+class ExpressionPrinter:
+    """Visitor
+
+    All printing logic is kept in a separate class and we do not modify DoubleExpression and AdditionExpression
+    This class has static method for Expression Printer"""
+
+    @staticmethod
+    def print_printer(expression: AdditionExpression | DoubleExpression, buffer: List):
+        # we don't know what type of expression we received, so we need to do type checking
+        if isinstance(expression, DoubleExpression):
+            buffer.append(str(expression.value))
+        elif isinstance(expression, AdditionExpression):
+            buffer.append('(')
+            ExpressionPrinter.print_printer(expression.left,
+                                            buffer)  # NIIIICE reuse method we are currently in recursively:
+            buffer.append('+')
+            ExpressionPrinter.print_printer(expression.right, buffer)
+            buffer.append(')')
+
+    # lambda function that take expression and lambda and adds the functionality in class definition time
+    logger.error(f"Expression . print not there yet {dir(Expression)}")
+    logger.error(f".print in Expression: {'print' in dir(Expression)}")
+
+    # TODO: WOW. 'lambda expression, buffer: ... ' expression IS SELF by FACT. that's why we can call
+    # e.print(buffer) -> and pass only buffer. As self would be passed automatically
+    Expression.print = lambda expression, buffer: ExpressionPrinter.print_printer(expression, buffer)
+    logger.error(f"Expression . print added {dir(Expression)}")
+    logger.error(f".print in Expression: {'print' in dir(Expression)}")
+    print(Expression.print)
+    print(inspect.getfullargspec(Expression.print))
+
+
+if __name__ == '__main__':
+    # 1 + (2 + 3)
+    e = AdditionExpression(DoubleExpression(1),
+                           AdditionExpression(DoubleExpression(2), DoubleExpression(3))
+                           )
+
+    buffer = []
+
+    # e will be passed as self
+    e.print(buffer)  # IDE will complain as print method is added in runtime
+
+    # Also will work:
+    buffer.append('\n')
+    # but here we need to pass e and buffer
+    ExpressionPrinter.print_printer(e, buffer)  # Call Visitor
+
+    print(''.join(buffer))
